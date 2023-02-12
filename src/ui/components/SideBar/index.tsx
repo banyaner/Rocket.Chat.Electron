d5b10d907ac990ab3cd805956f3cbe9ebeee7c1e
midsummer <1310009181@qq.com>
2023-02-12 20:47:07 +0800

    feat: 保留增加服务器功能用于测试

--- src/ui/components/SideBar/index.tsx
+++ src/ui/components/SideBar/index.tsx
@@ -10,6 +10,7 @@ import {
   SIDE_BAR_DOWNLOADS_BUTTON_CLICKED,
   SIDE_BAR_SETTINGS_BUTTON_CLICKED,
   SIDE_BAR_ADDRESS_BOOK_BUTTON_CLICKED,
+  SIDE_BAR_ADD_NEW_SERVER_CLICKED,
 } from '../../actions';
 import { useServers } from '../hooks/useServers';
 import ServerButton from './ServerButton';
@@ -41,7 +42,11 @@ export const SideBar: FC = () => {
   const handelAddressBookButtonClicked = (): void => {
     dispatch({ type: SIDE_BAR_ADDRESS_BOOK_BUTTON_CLICKED });
   };
-  
+
+  const handleAddServerButtonClicked = (): void => {
+    dispatch({ type: SIDE_BAR_ADD_NEW_SERVER_CLICKED });
+  };
+
   const { t } = useTranslation();
 
   const currentView = useSelector(({ currentView }: RootState) => currentView);
@@ -80,10 +85,18 @@ export const SideBar: FC = () => {
           onClick={handelAddressBookButtonClicked}
           isSelected={currentView === 'address-book'}
         >
-           {/* todo: address-book需要将"@rocket.chat/fuselage": "0.31.1"升级到0.31.22，但升级后有报错，之后可以使用本地构建版本替换。 */}
+          {/* todo: address-book需要将"@rocket.chat/fuselage": "0.31.1"升级到0.31.22，但升级后有报错，之后可以使用本地构建版本替换。 */}
           {/* <Icon name='address-book' /> */}
-          <Icon name='members' /> 
+          <Icon name='members' />
           <SidebarActionButtonLabel>{t('sidebar.addressBook')}</SidebarActionButtonLabel>
+        </SidebarActionButton>
+        <SidebarActionButton
+          onClick={handleAddServerButtonClicked}
+          isSelected={currentView === 'add-new-server'}
+        >
+          {/* todo: 保留暂时用于测试 */}
+          <Icon name='members' />
+          <SidebarActionButtonLabel>{t('sidebar.addNewServer')}</SidebarActionButtonLabel>
         </SidebarActionButton>
       </Content>
     </Wrapper>
